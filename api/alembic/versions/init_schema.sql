271054f4ab86b9701cafec40b46c6c362f2dfd43
Eledoux <lawanledoux@gmail.com>
2020-05-30 00:24:17 +0200

    remove config init_schema

--- api/alembic/versions/init_schema.sql
+++ api/alembic/versions/init_schema.sql
@@ -5,13 +5,7 @@
 -- Dumped from database version 10.1
 -- Dumped by pg_dump version 10.1
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
+
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
