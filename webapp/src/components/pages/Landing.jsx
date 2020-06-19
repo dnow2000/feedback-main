9e70534d5fb6ca45b6308b3b71abf5b33c6b9047
Eledoux <lawanledoux@gmail.com>
2020-06-20 01:14:37 +0200

    add rel to anchors

--- webapp/src/components/pages/Landing.jsx
+++ webapp/src/components/pages/Landing.jsx
@@ -94,26 +94,50 @@ const Landing = () => {
 
             <div className="links-container-big">
               <p className="h3">Community</p>
-              <a className="link" href="https://climatefeedback.org/community/" target="_blank">
+              <a
+                className="link"
+                href="https://climatefeedback.org/community/"
+                rel="noopener noreferrer"
+                target="_blank">
                 Climate Reviewers
               </a>
-              <a className="link" href="https://healthfeedback.org/community/" target="_blank">
+              <a
+                className="link"
+                href="https://healthfeedback.org/community/"
+                rel="noopener noreferrer"
+                target="_blank">
                 Health Reviewers
               </a>
-              <a className="link" href="https://sciencefeedback.co/for-scientists/" target="_blank">
+              <a
+                className="link"
+                href="https://sciencefeedback.co/for-scientists/"
+                rel="noopener noreferrer"
+                target="_blank">
                 Apply to become a reviewer
               </a>
             </div>
 
             <div className="links-container-big">
               <p className="h3">Organization</p>
-              <a className="link" href="https://sciencefeedback.co/about/" target="_blank">
+              <a
+                className="link"
+                href="https://sciencefeedback.co/about/"
+                rel="noopener noreferrer"
+                target="_blank">
                 About
               </a>
-              <a className="link" href="https://sciencefeedback.co/donate/" target="_blank">
+              <a
+                className="link"
+                href="https://sciencefeedback.co/donate/"
+                rel="noopener noreferrer"
+                target="_blank">
                 Support us
               </a>
-              <a className="link" href="https://sciencefeedback.co/community-standards/" target="_blank">
+              <a
+                className="link"
+                href="https://sciencefeedback.co/community-standards/"
+                rel="noopener noreferrer"
+                target="_blank">
                 Community standards
               </a>
             </div>
@@ -123,7 +147,11 @@ const Landing = () => {
               <NavLink className="link" to="/">
                 Press
               </NavLink>
-              <a className="link" href="https://sciencefeedback.co/contact-us/" target="_blank">
+              <a
+                className="link"
+                href="https://sciencefeedback.co/contact-us/"
+                rel="noopener noreferrer"
+                target="_blank">
                 Contact Us
               </a>
             </div>
