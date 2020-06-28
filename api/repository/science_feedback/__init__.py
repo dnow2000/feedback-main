f091f9d75804ec4025dd0010eee31e94dc235aac
Eledoux <lawanledoux@gmail.com>
2020-06-28 20:18:44 +0200

    reput all sync

--- api/repository/science_feedback/__init__.py
+++ api/repository/science_feedback/__init__.py
@@ -3,5 +3,5 @@ from repository.science_feedback.wordpress import sync as sync_wordpress
 
 
 def sync(max_records=None):
-    #sync_airtable(max_records=max_records)
+    sync_airtable(max_records=max_records)
     sync_wordpress()
