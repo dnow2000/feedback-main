9d455be471513c0a15f3b4cc25c4624cbae23c93
Eledoux <lawanledoux@gmail.com>
2020-06-29 23:04:36 +0200

    parse structure

--- api/domain/graph.py
+++ api/domain/graph.py
@@ -1,45 +1,56 @@
+from random import random, seed
 
 
-def graph_from_entity(entity):
-    return {
-        'nodes': [
-            {
-                'id': 'n0',
-                'label': 'A node',
-                'x': 0,
-                'y': 0,
-                'size': 3
-            },
-            {
-                'id': 'n1',
-                'label': 'Another node',
-                'x': 3,
-                'y': 1,
-                'size': 2
-            },
-            {
-                'id': 'n2',
-                'label': 'And a last one',
-                'x': 1,
-                'y': 3,
-                'size': 1
-            }
-        ],
-        'edges': [
-            {
-                'id': 'e0',
-                'source': 'n0',
-                'target': 'n1'
-            },
-            {
-                'id': 'e1',
-                'source': 'n1',
-                'target': 'n2'
-            },
-            {
-                'id': 'e2',
-                'source': 'n2',
-                'target': 'n0'
-            }
-        ]
-    }
+def node_type_from(entity):
+    return entity.__class__.__name__
+
+
+def node_id_from(entity):
+    return '{}_{}'.format(node_type_from(entity), entity.id)
+
+
+def label_from(entity):
+    return node_id_from(entity)
+
+
+def graph_from_entity(entity, node_ids=None, graph=None):
+    if not graph:
+        graph = {
+            'nodes': [],
+            'edges': []
+        }
+        node_ids = []
+        seed(1)
+
+    node_id = node_id_from(entity)
+
+    if node_id not in node_ids:
+        node = {
+            'label': label_from(entity),
+            'id': node_id_from(entity),
+            'type': node_type_from(entity),
+            'x': random(),
+            'y': random(),
+            'size': 3
+        }
+        node_ids.append(node_id)
+        graph['nodes'].append(node)
+
+        for key in entity.__mapper__.relationships.keys():
+            sub_entities = getattr(entity, key)
+            if not isinstance(sub_entities, list):
+                sub_entities = [sub_entities]
+            for sub_entity in sub_entities:
+                if sub_entity:
+                    sub_node_id = node_id_from(sub_entity)
+                    edge = {
+                        'id': '{}_{}'.format(node_id, sub_node_id),
+                        'source': node_id,
+                        'target': sub_node_id
+                    }
+                    graph['edges'].append(edge)
+                    graph_from_entity(sub_entity,
+                                      node_ids=node_ids,
+                                      graph=graph)
+
+    return graph
