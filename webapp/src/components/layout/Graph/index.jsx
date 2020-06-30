eb6fcca72214c51959424cec1031da4ecf7a1337
Eledoux <lawanledoux@gmail.com>
2020-06-30 23:26:51 +0200

    better use

--- webapp/src/components/layout/Graph/index.jsx
+++ webapp/src/components/layout/Graph/index.jsx
@@ -5,7 +5,6 @@ import React, { useEffect, useRef } from 'react'
 
 export default ({ graph }) => {
   const graphRef = useRef()
-  console.log({graph})
 
   useEffect(() => {
     if (!graph) return
@@ -16,7 +15,7 @@ export default ({ graph }) => {
 
     const width = graphRef.current.clientWidth
     const height = graphRef.current.clientHeight
-    const margin = {top:0, left:0, bottom:0, right:0 }
+    const margin = { bottom: 0, left: 0, right: 0, top: 0 }
     const chartWidth = width - (margin.left+margin.right)
     const chartHeight = height - (margin.top+margin.bottom)
     svg.attr('width', width)
@@ -27,7 +26,7 @@ export default ({ graph }) => {
 
     const simulation = d3Force.forceSimulation()
             .force('link', d3Force.forceLink().id(function(d) { return d.id }))
-            .force('collide',d3Force.forceCollide( function(d){return d.size + 8 }).iterations(16) )
+            .force('collide', d3Force.forceCollide( function(d){ return d.size + 8 }).iterations(16) )
             .force('charge', d3Force.forceManyBody())
             .force('center', d3Force.forceCenter(chartWidth / 2, chartHeight / 2))
             .force('y', d3Force.forceY(0))
@@ -41,12 +40,59 @@ export default ({ graph }) => {
             .append('line')
             .attr('stroke', 'black')
 
+    /*
     const node = svg.append('g')
             .attr('class', 'nodes')
-            .selectAll('circle')
+            .selectAll('rect')
             .data(nodes)
-            .enter().append('circle')
-            .attr('r', function(d){  return d.size })
+            .enter()
+            .append('rect')
+            .attr('width', 100)
+            .attr('height',10)
+    */
+
+    const node = svg.append('g')
+            .attr('class', 'nodes')
+            .selectAll('foreignObject')
+            .data(nodes)
+            .enter()
+            .append('g')
+
+    /*
+    node.append('rect')
+        .attr('width', 100)
+        .attr('height',10)
+        .attr('x', 0)
+        .attr('y', 0)
+    */
+
+    node.append('foreignObject')
+        .attr('width', 100)
+        .attr('height', 100)
+        .append('xhtml:div')
+        .html('<div> TEST </div>')
+
+            //
+
+    //node.append('circle')
+    //    .attr('r', function(d){ return d.size })
+
+
+            /*
+            .attr('width',10)
+            .attr('height',10)
+            .attr('x', function(d) { return d.x })
+            .attr('y', function(d) { return d.y })
+            .style('fill', 'red')
+            */
+    //const
+            //.append('foreignObject')
+            //.attr('width', 100)
+            //.attr('height', 10)
+            //.append('xhtml:div')
+            //.html('<h1>An HTML Foreign Object in SVG</h1><p>Lorem </p>.');
+
+
 
     const ticked = function() {
       link.attr('x1', function(d) { return d.source.x })
@@ -54,8 +100,9 @@ export default ({ graph }) => {
           .attr('x2', function(d) { return d.target.x })
           .attr('y2', function(d) { return d.target.y })
 
-      node.attr('cx', function(d) { return d.x })
-          .attr('cy', function(d) { return d.y })
+      //node.attr('x', function(d) { return d.x })
+      //    .attr('y', function(d) { return d.y })
+      node.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; });
     }
 
     simulation.nodes(nodes)
@@ -64,8 +111,6 @@ export default ({ graph }) => {
 
     simulation.force('link')
               .links(edges)
-
-    console.log({svg})
   }, [graph, graphRef])
 
 
