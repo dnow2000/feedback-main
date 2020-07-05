c1dcbc7938a0d08a91b2fd96870b77741dcfc2e7
Eledoux <lawanledoux@gmail.com>
2020-07-05 15:42:56 +0200

    fix cypress

--- webapp/cypress/integration/signin.test.js
+++ webapp/cypress/integration/signin.test.js
@@ -4,5 +4,14 @@ import { ROOT_PATH } from '../utils/config'
 describe('Signin', () => {
   it('can signin.', () => {
     cy.visit(`${ROOT_PATH}/signin`)
+
+    cy.get('input[name="identifier"]')
+      .type('fbtest.editor0@feedback.news')
+
+      cy.get('input[name="password"]')
+        .type('user@AZERTY123')
+
+    cy.get('button[type="submit"]')
+      .click()
   })
 })
--- webapp/cypress/utils/config.js
+++ webapp/cypress/utils/config.js
@@ -1,2 +1,3 @@
+export const APP_NAME = process.env.APP_NAME 
 export const API_URL = process.env.API_URL || 'http://localhost'
 export const ROOT_PATH = process.env.ROOT_PATH || 'http://localhost:3000'
