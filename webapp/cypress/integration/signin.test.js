62085dbef72fc6f37b98f77400067f4de072d0cc
Eledoux <lawanledoux@gmail.com>
2020-07-05 16:24:58 +0200

    fix signin

--- webapp/cypress/integration/signin.test.js
+++ webapp/cypress/integration/signin.test.js
@@ -7,16 +7,21 @@ import {
 
 
 describe('Signin', () => {
-  it('can signin.', () => {
+  it(`can signin.`, () => {
+
     cy.visit(`${ROOT_PATH}/signin`)
 
     cy.get('input[name="identifier"]')
       .type(`${COMMAND_NAME}test.editor0@${APP_NAME}.${TLD}`)
 
-      cy.get('input[name="password"]')
-        .type('user@AZERTY123')
+    cy.get('input[name="password"]')
+      .type('user@AZERTY123')
 
     cy.get('button[type="submit"]')
       .click()
+
+    cy.url()
+      .should('include', '/sources')
+
   })
 })
--- webapp/scripts/manager.js
+++ webapp/scripts/manager.js
@@ -18,15 +18,15 @@ program
   .version('0.1.0')
 
   .option('end2end', 'end2end')
+  .option('-t, --task [type]', 'open or run', 'open')
 
   .parse(process.argv)
 
 const { end2end, symlink, unsymlink } = program
 
 if (end2end) {
-  const { browser, debug, file } = program
-  const debugOption = debug ? '-d' : ''
+  const { task } = program
   const envOption = envConfig.split('\n').slice(0, -1).join(',')
-  const command = `NODE_ENV=development ./node_modules/.bin/cypress open --env=${envOption}`
+  const command = `NODE_ENV=development ./node_modules/.bin/cypress ${task} --env=${envOption}`
   childProcess.execSync(command, { stdio: [0, 1, 2] })
 }
