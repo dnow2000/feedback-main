995a3d8ed5c5de71ef0421f06a26c7cbd7c19ed4
Eledoux <lawanledoux@gmail.com>
2020-07-20 21:29:04 +0200

    appearances route

--- api/routes/appearances.py
+++ api/routes/appearances.py
@@ -1,16 +1,27 @@
-from flask import current_app as app, jsonify
+from flask import current_app as app, jsonify, request
 from sqlalchemy_api_handler import ApiHandler, \
                                    as_dict, \
                                    load_or_404
 
 from models.appearance import Appearance
+from models.verdict import Verdict
+from repository.appearances import keep_appearances_from_verdict
 from utils.rest import listify
 
 
 @app.route('/appearances', methods=['GET'])
 def get_appearances():
     query = Appearance.query
-    return listify(Appearance, query=query)
+
+    if 'verdictId' in request.args:
+        verdict = load_or_404(Verdict, request.args['verdictId'])
+        query = keep_appearances_from_verdict(query, verdict)
+
+    return listify(Appearance,
+                   page=request.args.get('page', 1),
+                   paginate=10,
+                   query=query,
+                   with_total_data_count=True)
 
 
 @app.route('/appearances/<appearance_id>', methods=['GET'])
