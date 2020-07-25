628945d449497b85d653233b6282633cfb751835
Eledoux <lawanledoux@gmail.com>
2020-07-25 16:15:27 +0200

    fix clock content

--- api/jobs/content.py
+++ api/jobs/content.py
@@ -22,38 +22,38 @@ CLOCK_SYNC_CONTENT_CONFIGS = [
 ]
 
 
-    """
-    # contents inserted since 1.7h - 0, do it every 20 minutes
-    {
-        'from_date': 100,
-        'to_date': 0,
-        'frequency': {'minute': '*/20'}
-    },
-    # contents inserted since 17h - 1.7h, do it every 2 hours
-    {
-        'from_date': 1000,
-        'to_date': 100,
-        'frequency': {'hour': '*/2', 'minute': '1'}
-    },
-    # contents inserted since 7days - 17h, do it every 7 days
-    {
-        'from_date': 10000,
-        'to_date': 1000,
-        'frequency': {'day': '*/7', 'hour': '0', 'minute': '1'}
-    },
-    # contents inserted since 70days - 7days, do it every 2 monthes
-    {
-        'from_date': 100000,
-        'to_date': 10000,
-        'frequency': {'month': '*/2', 'day': '1', 'hour': '0', 'minute': '1'}
-    },
-    # contents inserted since start - 70days, do it every 2 years
-    {
-        'from_date': None,
-        'to_date': 100000,
-        'frequency': {'year': '*/2', 'month': '1', 'day': '1', 'hour': '0', 'minute': '1'}
-    },
-    """
+"""
+# contents inserted since 1.7h - 0, do it every 20 minutes
+{
+    'from_date': 100,
+    'to_date': 0,
+    'frequency': {'minute': '*/20'}
+},
+# contents inserted since 17h - 1.7h, do it every 2 hours
+{
+    'from_date': 1000,
+    'to_date': 100,
+    'frequency': {'hour': '*/2', 'minute': '1'}
+},
+# contents inserted since 7days - 17h, do it every 7 days
+{
+    'from_date': 10000,
+    'to_date': 1000,
+    'frequency': {'day': '*/7', 'hour': '0', 'minute': '1'}
+},
+# contents inserted since 70days - 7days, do it every 2 monthes
+{
+    'from_date': 100000,
+    'to_date': 10000,
+    'frequency': {'month': '*/2', 'day': '1', 'hour': '0', 'minute': '1'}
+},
+# contents inserted since start - 70days, do it every 2 years
+{
+    'from_date': None,
+    'to_date': 100000,
+    'frequency': {'year': '*/2', 'month': '1', 'day': '1', 'hour': '0', 'minute': '1'}
+},
+"""
 
 JOBS = []
 
