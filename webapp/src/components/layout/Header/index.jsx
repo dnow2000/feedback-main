dcf7f0badd876d4059a1ec1751dd3eafe5f39b98
Quan Trinh <trinhquan1123@yahoo.com>
2020-07-28 04:22:37 +0800

    resize header, add support button & remove border around verdict-item in appearances

--- webapp/src/components/layout/Header/index.jsx
+++ webapp/src/components/layout/Header/index.jsx
@@ -51,17 +51,22 @@ export default ({ withLinks=false }) => {
           <Logo type="header" />
         </div>
 
-        <div className="flex-auto" />
         {withLinks && (
           <div className="right-content">
             <Navigations />
+            <a
+              className='support-us'
+              href='https://sciencefeedback.co/donate/'
+            >
+              {'Support us'}
+            </a>
             <Menu />
-            {showSignin && (
+            {false && showSignin && (
               <NavLink
                 className="button"
                 to="/signin"
               >
-                Sign in
+                {'Sign in'}
               </NavLink>
             )}
           </div>
