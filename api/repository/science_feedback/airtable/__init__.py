c01435737d6cdec5864de4c9098df59f2da69576
Quan Trinh <trinhquan1123@yahoo.com>
2020-08-27 15:55:13 +0800

    add correct args

--- api/repository/science_feedback/airtable/__init__.py
+++ api/repository/science_feedback/airtable/__init__.py
@@ -33,7 +33,7 @@ def entity_from_row_for(name, entity_dict, index):
     return entity_from_row_function(entity_dict, index)
 
 
-def sync_outdated_rows(name, max_records=None):
+def sync_outdated_rows(max_records=None):
     logger.info('sync science feedback outdated airtable data...')
     for name in NAME_TO_AIRTABLE:
         sync_for(name, formula='FIND("Out of sync", {Sync status})>0', max_records=max_records)
