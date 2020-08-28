53a129d447b360d1ffb7878d7706da0114bc68d0
heloisethero <thero.heloise@gmail.com>
2020-08-28 15:08:15 +0200

    add warning if no CrowdTangle key in the .env

--- api/domain/trendings/crowdtangle.py
+++ api/domain/trendings/crowdtangle.py
@@ -1,7 +1,11 @@
 import os
+from sqlalchemy_api_handler import logger
 
 CROWDTANGLE_API_URL = 'https://api.crowdtangle.com'
 CROWDTANGLE_API_KEY = os.environ.get('CROWDTANGLE_API_KEY')
+
+if CROWDTANGLE_API_KEY is None:
+    logger.warning('CROWDTANGLE_API_KEY is not defined in the env!')
 
 def facebook_shares_from_crowdtangle():
     return [CROWDTANGLE_API_URL, CROWDTANGLE_API_KEY]
