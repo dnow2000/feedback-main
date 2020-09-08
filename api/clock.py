8f8f776bf18298e96f6bcd331be9a30b6cc12fea
Quan Trinh <trinhquan1123@yahoo.com>
2020-09-08 17:24:20 +0800

    run CLOCK_APP with gunicorn

--- api/clock.py
+++ api/clock.py
@@ -7,18 +7,18 @@ from utils.config import IS_DEVELOPMENT
 from utils.setup import setup
 
 
-FLASK_APP = Flask(__name__)
+CLOCK_APP = Flask(__name__)
 
-setup(FLASK_APP, with_jobs=True)
+setup(CLOCK_APP, with_jobs=True)
 
 if __name__ == '__main__':
-    FLASK_APP.async_scheduler.start()
-    # FLASK_APP.background_scheduler.start()
-    atexit.register(lambda: FLASK_APP.async_scheduler.shutdown())
+    CLOCK_APP.async_scheduler.start()
+    # CLOCK_APP.background_scheduler.start()
+    atexit.register(lambda: CLOCK_APP.async_scheduler.shutdown())
 
     import_clock_routes()
 
-    FLASK_APP.run(debug=IS_DEVELOPMENT,
+    CLOCK_APP.run(debug=IS_DEVELOPMENT,
                   host='0.0.0.0',
                   port=5001,
                   use_reloader=True)
