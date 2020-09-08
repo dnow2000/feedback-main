7a0a65b6e4fb4772070564977ca4fb7098a6f269
Quan Trinh <trinhquan1123@yahoo.com>
2020-09-08 16:48:41 +0800

    typos of names

--- api/routes/jobs.py
+++ api/routes/jobs.py
@@ -4,12 +4,13 @@ from sqlalchemy_api_handler import as_dict, logger
 
 @app.route('/jobs', methods=['GET'])
 def get_all_jobs():
-    logger.info('Looking for jobs...')
     async_jobs = app.async_scheduler.get_jobs()
     background_jobs = app.background_scheduler.get_jobs()
-    jobs = {'async_jobs': [], 'background': []}
+    jobs = {'async': [], 'background': []}
+
     for job in async_jobs:
-        jobs['async_jobs'].append({'id': job.id, 'name': job.name})
+        jobs['async'].append({'id': job.id, 'name': job.name})
     for job in background_jobs:
-        jobs['background_jobs'].append({'id': job.id, 'name': job.name})
+        jobs['background'].append({'id': job.id, 'name': job.name})
+
     return jsonify(jobs)
