0b0388bf1ed0806fddcb11d85d2b83ad0dd10c2e
Quan Trinh <trinhquan1123@yahoo.com>
2020-09-11 18:05:59 +0800

    wip

---
+++ webapp/cypress/integration/appearances.test.js
@@ -0,0 +1,12 @@
+import {
+  APP_NAME,
+  COMMAND_NAME,
+  ROOT_PATH,
+  TLD
+} from '../utils/config'
+
+describe('appearances', () => {
+  it('should run', () => {
+    expect(true).to.equal(true)
+  })
+})
