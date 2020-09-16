06b6354a74783df83c11ab192dd023a98b419de0
Quan Trinh <trinhquan1123@yahoo.com>
2020-09-16 17:59:42 +0800

    add appearance page test

--- webapp/cypress/integration/appearances.test.js
+++ webapp/cypress/integration/appearances.test.js
@@ -1,12 +1,53 @@
-import {
-  APP_NAME,
-  COMMAND_NAME,
-  ROOT_PATH,
-  TLD
-} from '../utils/config'
+describe('appearances page', () => {
+  beforeEach(() => {
+    cy.visit('/landing')
+    cy.get('.verdict-item').first().click({force: true})
+  })
 
-describe('appearances', () => {
-  it('should run', () => {
-    expect(true).to.equal(true)
+  it('should be able navigate to verdict-item/appearances page', () => {
+    cy.url().should('include', '/appearances')
+  })
+
+  it('should have a verdict-item with details', () => {
+    cy.get('.verdict-item').find('h3')
+    cy.get('.tags a').contains('Read full review')
+  })
+
+  it('should have a links tab', () => {
+    cy.get('.tab-pane #links').contains(/.*\sLinks/)
+  })
+
+  it('should have one or more appearances', () => {
+    cy.get('a.appearance-item').its('length').should('be.gte', 1)
+  })
+})
+
+describe('each appearance', () => {
+  beforeEach(() => {
+    cy.visit('/landing')
+    cy.get('.verdict-item').first().click({force: true})
+  })
+
+  it('should be a link', () => {
+    cy.get('a.appearance-item')
+      .first()
+      .invoke('attr', 'href')
+      .should('not.be.empty')
+  })
+
+  it('should have a thumb image', () => {
+    cy.get('a.appearance-item').within(() => {
+      cy.get('.appearance-item-img')
+        .first()
+        .should('have.attr', 'src')
+    })
+  })
+
+  it('should have the appearance details', () => {
+    cy.get('a.appearance-item').within(() => {
+      cy.get('.appearance-title').first().should('exist')
+      cy.get('.appearance-source').first().should('exist')
+      cy.get('.appearance-url').first().should('exist')
+    })
   })
 })
