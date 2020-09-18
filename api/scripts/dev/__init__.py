76e11a6a9c9dc14c7a633e8fb2f38ad0598d89cc
Eledoux <lawanledoux@gmail.com>
2020-09-18 12:40:27 +0200

    rewrite scripts dev

---
+++ api/scripts/dev/__init__.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+from flask import current_app as app
+
+
+DEV_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
+
+
+@app.manager.option('-f',
+                    '--file',
+                    help='Execute a script in api/scripts/dev',
+                    default='')
+def dev(file):
+    exec(open(DEV_PATH / file).read())
