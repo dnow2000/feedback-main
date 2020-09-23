b8a52ddce836bfb47aa2c10f117907a5381d191b
Quan Trinh <trinhquan1123@yahoo.com>
2020-09-23 17:15:15 +0800

    bind migration content type

--- api/alembic/versions/2d177c515de0_add_type_to_content.py
+++ api/alembic/versions/2d177c515de0_add_type_to_content.py
@@ -18,6 +18,7 @@ depends_on = None
 
 def upgrade():
     content_type = sa.Enum('ARTICLE', 'POST', 'VIDEO', name='contenttype')
+    content_type.create(op.get_bind())
     op.add_column('content',
                   sa.Column('type',
                             content_type,
@@ -25,4 +26,6 @@ def upgrade():
 
 
 def downgrade():
+    content_type = sa.Enum(name='contenttype')
+    content_type.drop(op.get_bind())
     sa.drop_column('content', 'type')
