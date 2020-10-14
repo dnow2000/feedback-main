cdf4dcc58cf53b3eb5eb6a8ec2a6f93cb8e31140
Quan Trinh <trinhquan1123@yahoo.com>
2020-10-15 03:49:24 +0800

    working docker setup

--- api/celery_worker.py
+++ api/celery_worker.py
@@ -2,13 +2,14 @@ import os
 import celery
 
 
-app = celery.Celery(
-    'jobs',
+celery_app = celery.Celery(
+    f'{os.environ.get("APP_NAME")}-jobs',
     broker=os.environ.get('REDIS_URL'),
     backend=os.environ.get('REDIS_URL')
 )
 
 
-@app.task
+@celery_app.task
 def hello_world(name):
+    print('Reached Hello World Task')
     return f'Hello World, {name}!'
