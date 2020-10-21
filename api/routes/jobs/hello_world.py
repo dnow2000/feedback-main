e000b78a4ff8773f4b601281e4cebfe4a872f545
Quan Trinh <trinhquan1123@yahoo.com>
2020-10-21 20:38:59 +0800

    test flower container

--- api/routes/jobs/hello_world.py
+++ api/routes/jobs/hello_world.py
@@ -1,4 +1,7 @@
-from flask import current_app as app, jsonify, request
+import os
+import requests
+
+from flask import current_app as app, jsonify, redirect, request
 from tasks.hello_world import hello_world
 
 
@@ -8,5 +11,15 @@ def hello_world_job():
         name = request.args.get('name', 'Feedback')
         task = hello_world.apply_async(args=[name])
         return jsonify({'taks_id': task.id})
+    except Exception as e:
+        return jsonify(f'Exception: {e}'), 500
+
+
+@app.route('/jobs/<full_path>')
+def jobs_monitor(full_path):
+    try:
+        flower_url = os.environ.get('FLOWER_URL')
+        url = f'{flower_url}/api/{full_path}'
+        return jsonify(requests.get(url).json())
     except Exception as e:
         return jsonify(f'Exception: {e}'), 500
