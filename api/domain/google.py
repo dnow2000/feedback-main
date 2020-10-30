cc205c0e28d2335d1cf3e772e31344f17008a027
Quan Trinh <trinhquan1123@yahoo.com>
2020-10-30 16:42:08 +0800

    hotfix for exact terms search

--- api/domain/google.py
+++ api/domain/google.py
@@ -5,20 +5,23 @@ https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={s
 """
 
 import requests
+import urllib.parse
 
 from os import environ as env
 from sqlalchemy_api_handler.utils import logger
 
 API_KEY = env.get('GOOGLE_CUSTOM_SEARCH_API_KEY')
 ENGINE_ID = env.get('GOOGLE_CUSTOM_SEARCH_ENGINE_ID')
-BASE_URL = f'https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={ENGINE_ID}&exactTerms=true&q='
+BASE_URL = f'https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={ENGINE_ID}&q='
 
 
 def backlinks_from_url(url, count=10, page=1):
     backlinks = []
+    parsed = urllib.parse.urlparse(url)
+    exact_terms = f'{parsed.netloc}{parsed.path}'
     if page > 0:
         start = ((page - 1) * count) + 1
-        results = requests.get(f'{BASE_URL}{url}&count={count}&start={start}').json()
+        results = requests.get(f'{BASE_URL}{url}&exactTerms={exact_terms}&count={count}&start={start}').json()
         items = results['items']
         backlinks = _links_from_items(items)
 
@@ -30,6 +33,7 @@ def _links_from_items(items):
     for item in items:
         if item['kind'] == 'customsearch#result':
             link = {}
+            link['id'] = item['cacheId']
             link['title'] = item['title']
             link['htmlTitle'] = item['htmlTitle']
             link['url'] = item['link']
