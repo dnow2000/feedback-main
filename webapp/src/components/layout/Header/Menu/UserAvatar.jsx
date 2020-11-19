dbed813527760996dedef0fcfb4dba875b037efa
Mathieu <mathieu.abeille@gmail.com>
2020-11-19 17:10:38 +0100

    responsiveness & bugfix

--- webapp/src/components/layout/Header/Menu/UserAvatar.jsx
+++ webapp/src/components/layout/Header/Menu/UserAvatar.jsx
@@ -1,47 +1,42 @@
-import classnames from 'classnames'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
-import { selectCurrentUser } from 'with-react-redux-login'
+  import classnames from 'classnames'
+  import React, { useCallback } from 'react'
+  import { useDispatch, useSelector } from 'react-redux'
+  import { NavLink } from 'react-router-dom'
+  import { selectCurrentUser } from 'with-react-redux-login'
 
-import Avatar from 'components/layout/Avatar'
-import { closeMenu, showMenu } from 'reducers/menu'
-
-
-export default () => {
-  const dispatch = useDispatch()
+  import Avatar from 'components/layout/Avatar'
+  import { closeMenu, showMenu } from 'reducers/menu'
 
 
-  const currentUser = useSelector(selectCurrentUser)
+  export default () => {
+    const currentUser = useSelector(selectCurrentUser)
+    const dispatch = useDispatch()
+    const isMenuActive = useSelector(state => state.menu.isActive)
 
-  const isMenuActive = useSelector(state => state.menu.isActive)
+    const handleClick = useCallback(event => {
+      event.preventDefault()
+      if (!isMenuActive) {
+        dispatch(showMenu())
+      } else {
+        // For keyboard users.
+        // Not used for mouseclicks
+        // instead we capture clicks via dismiss overlay
+        dispatch(closeMenu())
+      }
+  }, [dispatch, isMenuActive])
 
-  const isAtTop = useSelector(state => state.scroll.isAtTop)
 
-
-
-  return (
-    <div className="user-avatar">
-      <NavLink
-        className={classnames('link', {
-          'is-active': isMenuActive
-        })}
-        disabled={!isAtTop}
-        to='#footer'
-        onClick={event => {
-          event.preventDefault()
-          if (!isMenuActive && isAtTop) {
-            dispatch(showMenu())
-          } else {
-            // For keyboard users.
-            // Not used for mouseclicks
-            // instead we capture clicks via dismiss overlay
-            dispatch(closeMenu())
-          }
-        }}
-      >
-        <Avatar user={currentUser} />
-      </NavLink>
-    </div>
-  )
-}
+    return (
+      <div className="user-avatar">
+        <NavLink
+          className={classnames('link', {
+            'is-active': isMenuActive
+          })}
+          onClick={handleClick}
+          to='#footer'
+        >
+          <Avatar user={currentUser} />
+        </NavLink>
+      </div>
+    )
+  }
